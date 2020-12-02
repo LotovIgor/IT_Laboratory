30c1c7297e8a4ab8cd76a7030b00bba6e39ece3b
LotovIgor <ilotov@ngs.ru>
2020-12-02 11:54:13 +0700

    1 and 2

--- Pandas_first.py
+++ Pandas_first.py
@@ -1,1 +1,5 @@
-
+import pandas
+data = pandas.read_csv('titanic.csv', index_col = 'PassengerId')
+print(data['Sex'].value_counts()['male'], end=' ')
+print(data['Sex'].value_counts()['female'])
+print(round(data['Survived'].value_counts()[1] / data['Survived'].count() * 100, 2))
