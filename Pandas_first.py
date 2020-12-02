ce3575568051f6eb13872fab7dbbfe9f1ae54af5
LotovIgor <ilotov@ngs.ru>
2020-12-02 13:19:06 +0700

    6

--- Pandas_first.py
+++ Pandas_first.py
@@ -13,3 +13,12 @@ d = data.sort_values('Age', ascending=False).reset_index()['Age']
 print((d[n // 2 - 1] + d[n // 2]) / 2)
 
 print(data[['Parch', 'SibSp']].corr())
+
+first_name = []
+for i in range(len(data)):
+    begin = data['Name'][i + 1].find('.') + 2
+    end = data['Name'][i + 1].find(' ', begin)
+    if data['Sex'][i + 1] == 'female':
+        first_name.append(data['Name'][i + 1][begin:end])
+names = pandas.Series(first_name)
+print(names.value_counts())
